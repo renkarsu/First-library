f7ee9716e9b27b99edc02247f6a337efb1119b2c
renkarsu <42439654+renkarsu@users.noreply.github.com>
2019-08-25 00:36:24 +0900

    endl to \n

--- TriangleTest.cpp
+++ TriangleTest.cpp
@@ -4,26 +4,26 @@
 //g++ -std=c++17 -Wall --pedantic-errors TriangleTest.cpp -o triangle
 int main(){
     //test 'hello.h'
-    std::cout << "Test Hello()" << std::endl;
+    std::cout << "Test Hello()" << '\n';
     brahman::Hello();
-    std::cout << std::endl;
+    std::cout << '\n';
 
     //test 'area.h'
     constexpr brahman::Triangle<double> tri(2.5, 4.2, 5.0);
 
-    std::cout << "Input data: " << tri.GetAB() << " " << tri.GetBC() << " " << tri.GetCA() << std::endl << std::endl;
+    std::cout << "Input data: " << tri.GetAB() << " " << tri.GetBC() << " " << tri.GetCA() << '\n' << '\n';
 
-    std::cout << "Test GetAB(), GetBC(), GetCA()" << std::endl;
-    std::cout << "tri.GetAB(): " << tri.GetAB() << std::endl;
-    std::cout << "tri.GetBC(): " << tri.GetBC() << std::endl;
-    std::cout << "tri.GetCA(): " << tri.GetCA() << std::endl << std::endl;
+    std::cout << "Test GetAB(), GetBC(), GetCA()" << '\n';
+    std::cout << "tri.GetAB(): " << tri.GetAB() << '\n';
+    std::cout << "tri.GetBC(): " << tri.GetBC() << '\n';
+    std::cout << "tri.GetCA(): " << tri.GetCA() << '\n' << '\n';
 
-    std::cout << "Test GetArea()" << std::endl;
-    std::cout << "tri.GetArea(): " << tri.GetArea() << std::endl << std::endl;
+    std::cout << "Test GetArea()" << '\n';
+    std::cout << "tri.GetArea(): " << tri.GetArea() << '\n' << '\n';
 
-    std::cout << "Test GetShape()" << std::endl;
-    std::cout << "tri.GetShape(): " << tri.GetShape() << std::endl << std::endl;
+    std::cout << "Test GetShape()" << '\n';
+    std::cout << "tri.GetShape(): " << tri.GetShape() << '\n' << '\n';
 
-    std::cout << "Test PrintCondition()" << std::endl;
+    std::cout << "Test PrintCondition()" << '\n';
     std::cout << tri;
 }
